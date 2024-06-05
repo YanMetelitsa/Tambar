5a266437ead27753b98620b73bd5673aa398737e
Yan Metelitsa <yan.metelitsa@gmail.com>
2024-06-05 20:25:35 +0300

    WP 6.5.4

--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === Tambar – Bottom Admin Bar ===
 Contributors: yanmetelitsa
 Tags: adminbar, admin, bar, position, bottom
-Tested up to: 6.5.3
+Tested up to: 6.5.4
 Requires at least: 6.4
 Stable tag: 2.2.0
 License: GPLv3
--- tambar.php
+++ tambar.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Tambar – Bottom Admin Bar
  * Description:       Plugin that allows to change position of the admin bar.
  * Version:           2.2.0
- * Tested up to:      6.5.3
+ * Tested up to:      6.5.4
  * Requires at least: 6.4
  * Author:            Yan Metelitsa
  * Author URI:        https://yanmet.com/
